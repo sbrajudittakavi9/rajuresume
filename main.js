cc059fc39dc63e0dd0e77a90a09e26c0b0234b5e
sbrajudittakavi9 <sbrajudittakavi9@gmail.com>
2018-07-14 19:36:47 +0530

    submit after change

--- main.js
+++ main.js
@@ -40,11 +40,14 @@ function career(careerInfo) {
     hd.textContent = "Career Objective";
     childTwo.appendChild(hd);
     var hr = document.createElement("hr");
+    var but = document.createElement("button");
+    but.value = "CareerInfo";
+    childTwo.appendChild(but);
     childTwo.appendChild(hr);
-    var careerP = document.createElement("p");//creating html elements
+    /*var careerP = document.createElement("p");//creating html elements
     careerP.textContent = careerInfo.info;
     childTwo.appendChild(careerP);
-
+*/
 }
 function education(educationInfo) {
     var hd = document.createElement("h4");
